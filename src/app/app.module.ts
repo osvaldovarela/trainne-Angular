f96676d0246e16ce403426ab16b5163133cd3bd8
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2022-03-04 02:47:40 -0300

    nuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { LogoApComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    LogoApComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
